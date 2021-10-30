8ea10e010d223e6c99fee8a60f452577d7f02c3e
thevioletmaniac <thevioletmaniac@gmail.com>
2021-10-30 16:57:51 +0600

    Adjusted data fetching

--- src/services/dictionary/dictionary.js
+++ src/services/dictionary/dictionary.js
@@ -1,13 +1,7 @@
-import { StatusCodes } from 'http-status-codes';
-
 export const getDefinitionByWord = async (word) => {
-  const dictioneryEndpoint = ' https://api.dictionaryapi.dev/api/v2/entries/en';
+  const dictionaryEndpoint = ' https://api.dictionaryapi.dev/api/v2/entries/en';
 
-  const result = await fetch(`${dictioneryEndpoint}/${word}`).then((response) => {
-    if (response.status === StatusCodes.OK) {
-      return response.json();
-    }
+  return await fetch(`${dictionaryEndpoint}/${word}`).then((response) => {
+    return response.json();
   });
-
-  return result;
 };
