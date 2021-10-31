4cda3ec70faafe6bc6fc53d1a0379356e4ad67a6
thevioletmaniac <thevioletmaniac@gmail.com>
2021-10-31 17:09:12 +0600

    Implemented dictionary logic

--- src/App.js
+++ src/App.js
@@ -1,53 +1,104 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
+import Layout from './components/base/Layout';
+import Message from './components/base/Message';
+import Spinner from './components/base/Spinner';
+import Overlay from './components/dictionary/Overlay/Overlay';
+import SearchForm from './components/dictionary/SearchForm';
+import Word from './components/dictionary/Word';
 import { getDefinitionByWord } from './services/dictionary';
 
 const App = () => {
-  const [isLoaded, setLoaded] = useState(false);
   const [definitions, setDefinitions] = useState('');
   const [phonetic, setPhonetic] = useState('');
-  const [word, setWord] = useState('bun');
-  const [background, setBackground] = useState('');
+  const [word, setWord] = useState({
+    value: '',
+    localValue: '',
+  });
+
+  const [status, setStatus] = useState({
+    isWordLoaded: false,
+    isFirstLoading: true,
+    isWordChanged: false,
+    isFailed: false,
+  });
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchDefinition = async () => {
-      const data = await getDefinitionByWord(word);
+      const data = await getDefinitionByWord(word.value);
 
-      setLoaded(true);
-      setDefinitions(data[0].meanings[0].definitions);
-      setPhonetic(data[0].phonetic);
+      if (!Array.isArray(data)) {
+        setError(data);
+        setStatus((prevStatus) => ({
+          ...prevStatus,
+          isFailed: true,
+        }));
+      } else {
+        setDefinitions(data[0].meanings[0].definitions);
+        setPhonetic(data[0].phonetic);
+        setStatus((prevStatus) => ({
+          ...prevStatus,
+          isFailed: false,
+        }));
+      }
+
+      setStatus((prevStatus) => ({
+        ...prevStatus,
+        isLoaded: true,
+        isLoading: false,
+        isFirstLoading: false,
+      }));
     };
 
-    if (!isLoaded) fetchDefinition();
+    if (status.isLoading) fetchDefinition();
+  }, [status.isLoading]);
+
+  const changeWord = useCallback((e) => {
+    const value = e.target.value;
+
+    setWord((prevWord) => ({
+      ...prevWord,
+      localValue: value,
+    }));
   }, []);
 
-  useEffect(() => {
-    const endpoint = 'https://source.unsplash.com/1600x900';
+  const searchWord = useCallback(
+    (e) => {
+      e.preventDefault();
 
-    setBackground(`${endpoint}/?${word}`);
-  }, [word]);
+      if (!word.localValue) return;
 
-  const renderDefinitions = useMemo(() => {
-    if (!isLoaded) return;
+      setWord((prevWord) => ({
+        ...prevWord,
+        value: word.localValue,
+      }));
 
-    return definitions?.map((definition) => {
-      return (
-        <li className="definition" key={definition.definition}>
-          {definition.definition}
-        </li>
-      );
-    });
-  }, [definitions]);
+      setStatus((prevStatus) => ({
+        ...prevStatus,
+        isLoading: true,
+      }));
+    },
+    [word]
+  );
 
   return (
-    <div className="section">
-      <div className="content">
-        <h1 className="word">{word}</h1>
-        <h2 className="phonetic">{phonetic}</h2>
-        <ul className="definitions">{renderDefinitions}</ul>
-      </div>
-      <div className="overlay" style={{ backgroundImage: `url("${background}")` }} />
-    </div>
+    <Layout>
+      <SearchForm word={word.localValue} searchWord={searchWord} changeWord={changeWord} />
+      {status.isLoading && (
+        <div className="layout__spinner">
+          <Spinner />
+        </div>
+      )}
+      {status.isLoaded && !status.isFailed && !status.isLoading && (
+        <Word word={word.value} phonetic={phonetic} definitions={definitions} />
+      )}
+      {status.isFirstLoading && !status.isLoading && (
+        <Message text="Enter any word in the input above to get word definition." />
+      )}
+      {status.isFailed && <Message title={error.title} text={error.message} />}
+      {status.isLoaded && <Overlay word={word.value} />}
+    </Layout>
   );
 };
 
