d95a638f9893ece84d8ff5f7ce7abbb8f772c281
thevioletmaniac <thevioletmaniac@gmail.com>
2022-07-29 19:15:14 +0600

    Updates

---
+++ precache-manifest.18bd7902646e69801ceee28fba1807c4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73c444883acd5f6a36dd8730a47a1fe",
+    "url": "/react-dictionary-app/index.html"
+  },
+  {
+    "revision": "77e0abfadd28db6c843d",
+    "url": "/react-dictionary-app/static/css/2.0b3cff39.chunk.css"
+  },
+  {
+    "revision": "aa9768b98d89460e66c9",
+    "url": "/react-dictionary-app/static/css/main.8954a492.chunk.css"
+  },
+  {
+    "revision": "77e0abfadd28db6c843d",
+    "url": "/react-dictionary-app/static/js/2.f5862876.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/react-dictionary-app/static/js/2.f5862876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa9768b98d89460e66c9",
+    "url": "/react-dictionary-app/static/js/main.d07f034e.chunk.js"
+  },
+  {
+    "revision": "42d23ea0d2fbc729a53f",
+    "url": "/react-dictionary-app/static/js/runtime-main.04905395.js"
+  },
+  {
+    "revision": "d14ad1035ae6da4e5a71eca362a8d696",
+    "url": "/react-dictionary-app/static/media/Montserrat-Bold.d14ad103.ttf"
+  },
+  {
+    "revision": "aca6287f22eef510c1e622c97bb1e1e1",
+    "url": "/react-dictionary-app/static/media/Montserrat-Medium.aca6287f.ttf"
+  },
+  {
+    "revision": "34de1239b12123b85ff1a68b58835a1f",
+    "url": "/react-dictionary-app/static/media/Montserrat-Regular.34de1239.ttf"
+  },
+  {
+    "revision": "7ffeec2b4edb434d393875ffbe633c30",
+    "url": "/react-dictionary-app/static/media/Montserrat-SemiBold.7ffeec2b.ttf"
+  }
+]);
